97dd42d2d92c2d93e85e42b7606b17fb2db3c926
Prabodashan <prabodashan14@gmail.com>
2023-03-29 02:37:50 +0530

    added Custom Meta Component

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,7 +1,7 @@
 import { server } from "../../../config";
 import Link from "next/link";
 import { useRouter } from "next/router";
-// import Meta from "../../../components/Meta";
+import Meta from "../../../components/Meta";
 
 const article = ({ article }) => {
   //   const router = useRouter();
@@ -9,7 +9,7 @@ const article = ({ article }) => {
 
   return (
     <>
-      {/* <Meta title={article.title} description={article.excerpt} /> */}
+      <Meta title={article.title} description={article.excerpt} />
       <h1>{article.title}</h1>
       <p>{article.body}</p>
       <br />
